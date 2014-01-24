321d60f12594ee3960c1a3e70ec4e6a8ab256b86
Rico Sennrich <rico.sennrich@gmx.ch>
2014-01-24 12:16:57 +0000

    new markup for old spelling variants

--- extract_from_wiki_dump.py
+++ extract_from_wiki_dump.py
@@ -144,7 +144,7 @@ def extractFromWikidump(wikidump_filepath=None):
             i += 1
 
             # extract spelling variations
-            if ('{{Alternative Schreibweisen}}' in line or '{{Alte Rechtschreibung}}' in line) and i < len(text):
+            if ('{{Alternative Schreibweisen}}' in line or '{{Alte Rechtschreibung}}' in line or '{{Veraltete Schreibweisen}}' in line) and i < len(text):
                 alt_spellings = set(extractor_alt_spelling.findall(text[i]))
                 alt_spellings.difference_update(set(['Schweiz', 'Österreich', 'Liechtenstein', 'Deutschland'])) # names of regions where spelling variation occurs are sometimes extracted by accident
                 if 'alt_spelling' in entry:
